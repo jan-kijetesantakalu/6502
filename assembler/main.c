7004ae99fdb6ad63637e2750981dbe3a23536ffa
44779 <root@many-wholesome.co.uk>
2024-03-27 15:06:53 +0000

    asm test

--- assembler/main.c
+++ assembler/main.c
@@ -1,8 +1,27 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LINE_SIZE 255
 
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
 		printf("Usage: %s {input}.asm {output}.bin", argv[0]);
+		return -1;
 	}
+	FILE *asmrawfp = fopen(argv[1], "r");
+	if (asmrawfp == NULL) {
+		printf("NULL FP");	
+		return -1;
+	}
+	
+	char line[LINE_SIZE];
+	while (fgets(line, LINE_SIZE+1, asmrawfp) != NULL) {
+        	printf("%s", line);
+	}
+
+	fclose(asmrawfp);
+	return 0;
 }
--- assembler/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-	if (argc != 3) {
-		printf("Usage: %s {input}.asm {output}.bin", argv[0]);
-	}
-}
