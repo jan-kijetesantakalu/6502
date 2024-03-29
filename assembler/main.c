3a41fd836b3ec63b76db1e186308ea0817de8aea
44779 <root@many-wholesome.co.uk>
2024-03-29 22:10:00 +0000

    started making parse return instruction information

--- assembler/main.c
+++ assembler/main.c
@@ -6,52 +6,85 @@
 
 #define LINE_SIZE 64
 
+enum mode {
+	IMM,
+	ABS,
+	ZP_REL,
+	IND,
+	ABS_X,
+	ABS_Y,
+	ZP_X,
+	ZP_Y,
+	ZP_X_IND,
+	ZP_IND_Y,
+	IMP
+};
+
+struct instruction {
+	char SYM[4];
+	enum mode mode;
+	int operand;
+};
 
 
-int parse_line(char *line, FILE* fp) {
+struct instruction parse_line(char *line, FILE* fp) {
 	struct slre_cap SYM_cap;
 	if (slre_match("([A-Z][A-Z][A-Z])", line, strlen(line), &SYM_cap, 1, SLRE_IGNORE_CASE) < 0)
-		return -1;
+		exit(-1);
 	printf("found symbol: [%.*s]\n", SYM_cap.len, SYM_cap.ptr);
 	
 	struct slre_cap arg_cap;
+	
+	struct instruction instr;
+
+	memcpy(instr.SYM, SYM_cap.ptr, 3);
 
 	if (slre_match("\\s#(\\S*)\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found imm: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = IMM;
 	}
 	else if (slre_match("\\s(\\$\\S\\S\\S\\S)\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {	
 		printf("found abs addr: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ABS;
 	}
 	else if (slre_match("\\s(\\$\\S\\S)\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found zp/rel: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ZP_REL;
 	}
 	else if (slre_match("\\s\\((\\$\\S\\S\\S\\S)\\)\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found ind addr: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = IND;
 	}
 	else if (slre_match("\\s(\\$\\S\\S\\S\\S),X\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found abs idx X: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ABS_X;
 	}
 	else if (slre_match("\\s(\\$\\S\\S\\S\\S),Y\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found abs idx Y: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ABS_Y;
 	}
 	else if (slre_match("\\s(\\$\\S\\S),X\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found zp idx X: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ZP_X;
 	}
 	else if (slre_match("\\s(\\$\\S\\S),Y\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found zp idx Y: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ZP_Y;
 	}
 	else if (slre_match("\\s\\((\\$\\S\\S),X\\)\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found zp idx X ind: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ZP_X_IND;
 	}
 	else if (slre_match("\\s\\((\\$\\S\\S)\\),Y\\s*$", line, strlen(line), &arg_cap, 1, SLRE_IGNORE_CASE) > 0) {
 		printf("found zp ind idx Y: [%.*s]\n", arg_cap.len, arg_cap.ptr);
+		instr.mode = ZP_IND_Y;
 	}
-
 	else {
 		printf("found no arguments, impl\n");
+		instr.mode = IMP;
 	}
 
-	return 0;
+	return instr;
 }
 
 int main(int argc, char **argv) {
@@ -75,8 +108,7 @@ int main(int argc, char **argv) {
 	while (fgets(line, LINE_SIZE+1, asmrawfp) != NULL) {
 		line[strlen(line)-1]='\0';
 		printf("parsing line:\n[\n%s\n]\n", line);
-		if (parse_line(line, binfp) < 0)
-			printf("error parsing line\n");
+		struct instruction data = parse_line(line, binfp);
 	}
 
 	fclose(asmrawfp);
