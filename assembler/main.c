7402ef5e5e4d884506bcb6cd1e3380de483409b3
44779 <root@many-wholesome.co.uk>
2024-03-30 14:23:12 +0000

    minor fix

--- assembler/main.c
+++ assembler/main.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include "./lib/slre.h"
@@ -94,13 +93,13 @@ struct instruction parse_line(char *line, FILE* fp) {
 	memcpy(operand_str, arg_cap.ptr, arg_cap.len);
 	
 	if (operand_str[0] == '$') { //hex
-		instr.operand = (short)strtol(operand_str+1, NULL, 16);
+		instr.operand = (unsigned short)strtol(operand_str+1, NULL, 16);
 	}
 	else if (operand_str[0] == '%') { //bin
-		instr.operand = (short)strtol(operand_str+1, NULL, 2);
+		instr.operand = (unsigned short)strtol(operand_str+1, NULL, 2);
 	}
 	else { //dec
-		instr.operand = (short)strtol(operand_str, NULL, 10);
+		instr.operand = (unsigned short)strtol(operand_str, NULL, 10);
 	}
 
 	printf("opstr: %s -> val: %d\n", operand_str, instr.operand);
