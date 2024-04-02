b6a890e5ec1247b35908282c7c907c03bdd965e2
sundaylunch <sundaylunch@DESKTOP-66V8638>
2024-04-02 19:26:06 +0100

    test14

--- assembler/asmprograms/test14.asm
+++ assembler/asmprograms/test14.asm
@@ -1,8 +1,8 @@
-; Calculates the 8 bit root and 9 bit remainder of a 16 bit unsigned integer in
-; Numberl/Numberh. The result is always in the range 0 to 255 and is held in
-; Root, the remainder is in the range 0 to 511 and is held in Reml/Remh
+; Calculates the 8 bit $F6 and 9 bit remainder of a 16 bit unsigned integer in
+; $F0/$F1. The result is always in the range 0 to 255 and is held in
+; $F6, the remainder is in the range 0 to 511 and is held in $F2/$F3
 ;
-; partial results are held in templ/temph
+; partial results are held in $F4/$F5
 ;
 ; This routine is the complement to the integer square program.
 ;
@@ -10,65 +10,55 @@
 
 ; variables - must be in RAM
 
-Numberl		= $F0		; number to find square root of low byte
-Numberh		= Numberl+1	; number to find square root of high byte
-Reml		= $F2		; remainder low byte
-Remh		= Reml+1	; remainder high byte
-templ		= $F4		; temp partial low byte
-temph		= templ+1	; temp partial high byte
-Root		= $F6		; square root
-
-	*= $8000		; can be anywhere, ROM or RAM
-
-SqRoot
+SqRoot:
 	LDA	#$00		; clear A
-	STA	Reml		; clear remainder low byte
-	STA	Remh		; clear remainder high byte
-	STA	Root		; clear Root
+	STA	$F2		; clear remainder low byte
+	STA	$F3		; clear remainder high byte
+	STA	$F6		; clear $F6
 	LDX	#$08		; 8 pairs of bits to do
-Loop
-	ASL	Root		; Root = Root * 2
+Loop:
+	ASL	$F6		; $F6 = $F6 * 2
 
-	ASL	Numberl		; shift highest bit of number ..
-	ROL	Numberh		;
-	ROL	Reml		; .. into remainder
-	ROL	Remh		;
+	ASL	$F0		; shift highest bit of number ..
+	ROL	$F1		;
+	ROL	$F2		; .. into remainder
+	ROL	$F3		;
 
-	ASL	Numberl		; shift highest bit of number ..
-	ROL	Numberh		;
-	ROL	Reml		; .. into remainder
-	ROL	Remh		;
+	ASL	$F0		; shift highest bit of number ..
+	ROL	$F1		;
+	ROL	$F2		; .. into remainder
+	ROL	$F3		;
 
-	LDA	Root		; copy Root ..
-	STA	templ		; .. to templ
+	LDA	$F6		; copy $F6 ..
+	STA	$F4		; .. to $F4
 	LDA	#$00		; clear byte
-	STA	temph		; clear temp high byte
+	STA	$F5		; clear temp high byte
 
 	SEC			; +1
-	ROL	templ		; temp = temp * 2 + 1
-	ROL	temph		;
+	ROL	$F4		; temp = temp * 2 + 1
+	ROL	$F5		;
 
-	LDA	Remh		; get remainder high byte
-	CMP	temph		; comapre with partial high byte
+	LDA	$F3		; get remainder high byte
+	CMP	$F5		; comapre with partial high byte
 	BCC	Next		; skip sub if remainder high byte smaller
 
 	BNE	Subtr		; do sub if <> (must be remainder>partial !)
 
-	LDA	Reml		; get remainder low byte
-	CMP	templ		; comapre with partial low byte
+	LDA	$F2		; get remainder low byte
+	CMP	$F4		; comapre with partial low byte
 	BCC	Next		; skip sub if remainder low byte smaller
 
 				; else remainder>=partial so subtract then
-				; and add 1 to root. carry is always set here
-Subtr
-	LDA	Reml		; get remainder low byte
-	SBC	templ		; subtract partial low byte
-	STA	Reml		; save remainder low byte
-	LDA	Remh		; get remainder high byte
-	SBC	temph		; subtract partial high byte
-	STA	Remh		; save remainder high byte
+				; and add 1 to $F6. carry is always set here
+Subtr:
+	LDA	$F2		; get remainder low byte
+	SBC	$F4		; subtract partial low byte
+	STA	$F2		; save remainder low byte
+	LDA	$F3		; get remainder high byte
+	SBC	$F5		; subtract partial high byte
+	STA	$F3		; save remainder high byte
 
-	INC	Root		; increment Root
+	INC	$F6		; increment $F6
 Next
 	DEX			; decrement bit pair count
 	BNE	Loop		; loop if not all done
