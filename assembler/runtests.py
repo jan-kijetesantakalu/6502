644668d834060a7ebf806c46d7d4a96795abb063
jan-kijetesantakalu <root@many-wholesome.co.uk>
2024-04-02 14:13:08 +0100

    test runner

--- assembler/runtests.py
+++ assembler/runtests.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     print("making:")
     os.system("make")
     print("running tests:")
+    keep = []
     for test in tests:
         os.system(f"(./asm.exe {test} {test.split('/')[-1].split('.')[-2]}.bin.tmp) >> ./log.tmp")
         with open(test.split('/')[-1].split('.')[-2] +".bin.tmp", 'rb') as f:
@@ -16,16 +17,21 @@ if __name__ == "__main__":
             for ans in anss:
                 if ans.split('/')[-1].split('.')[-3] == test.split('/')[-1].split('.')[-2]:
                     tested = True
-                    with open(ans) as fa:
+                    with open(ans, 'rb') as fa:
                         if (fa.read() == f.read()):
                             print(f"\033[92m{test} passed")
                         else:
                             print(f"\033[91m{test} failed")
+                            keep.append(test.split('/')[-1].split('.')[-2] + '.bin.tmp')
             if tested:
                 continue
             print(f"\033[93m{test} answer [{testdir + test.split('/')[-1].split('.')[-2]+'.bin.ans'}] not found")
     print("\033[0mcleaning up:")
     for f in glob.glob("*.tmp"):
+        if f in keep:
+            print(f"\033[96mkeeping failed test {f} as {'.'.join(f.split('.')[:-1])}")
+            os.system(f'mv {f} {".".join(f.split(".")[:-1])}')
+            continue
         print(f"\033[96mrm'ing {f}")
         os.system(f"rm {f}")
     print("\033[0mcomplete")
